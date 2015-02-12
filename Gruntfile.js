183ee079be544a60ed7672c6232c157c9013af2f
Hector Leon <hectorleon.designer@gmail.com>
2015-02-12 12:19:23 +0100

    Create build version

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,29 +4,61 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
-      build: {
-        src: 'js/*.js',
-        dest: 'build/main.min.js'
-      }
-    },
+
     sass: {
       dist: {
         files: {
          'build/main.css':'scss/main.scss'
         }
       }
+    },
+
+    uglify: {
+      options: {
+        mangle: false,
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      my_target: {
+        files: {
+          'build/main.min.js': ['js/*.js', 'bower_components/bower-tinder-js/*.js']
+        }
+      }
+    },
+
+    processhtml: {
+      dist: {
+        options: {
+          process: true,
+          data: {
+            title: 'My Angular app',
+            message: 'This is production distribution'
+          }
+        },
+        files: {
+          'build/index.html': ['index.html']
+        }
+      }
+    },
+
+    copy: {
+      main: {
+        src: 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+        dest: 'build/bootstrap.min.css',
+      }
     }
+
   });
 
-
-  // Load the plugin that generates css from sass.
+  // Load the plugin tasks
+      // generates css from sass.
   grunt.loadNpmTasks('grunt-contrib-sass');
-  // Load the plugin that provides the "uglify" task.
+      // provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  // Default task(s).
-  grunt.registerTask('default', ['uglify', 'sass']);
+      // Default task(s).
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  // Custom tasks
+  grunt.registerTask('build', ['processhtml']);
+  grunt.registerTask('default', ['uglify', 'sass', 'copy']);
 };
